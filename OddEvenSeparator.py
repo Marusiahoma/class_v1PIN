4758afc2170277035e764f8ddac9d7d2885f872b
macbookair <m2003@tut.by>
2020-11-05 16:55:17 +0300

    4

--- OddEvenSeparator.py
+++ OddEvenSeparator.py
@@ -1,4 +1,20 @@
 class OddEvenSeparator:
     def __init__(self):
         self.odd = []
+        self.up = []
+        self.over = []
+
     def add_number(self, n):
+        self.odd.append(n)
+
+    def even(self):
+        for i in range(len(self.odd)):
+            if i % 2 == 0:
+                self.up.append(i)
+        return self.up
+
+    def odd(self):
+        for i in range(len(self.odd)):
+            if i % 2 != 0:
+                self.over.append(i)
+        return self.over
